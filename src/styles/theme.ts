dbd0689168f3eb8980a0e8f8b30e5b7f9ecf6bf6
Takashi Makimoto <mackie@beehive-dev.com>
2022-11-26 19:48:46 +0900

    グローバルスタイルの初期バージョンを追加 (#1)

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,9 +1,11 @@
 import { MantineThemeOverride } from '@mantine/core';
 import colors from './colors';
+import globalStyles from './globalStyles';
 
 const theme: MantineThemeOverride = {
   colorScheme: 'light',
   colors: colors,
+  globalStyles: globalStyles,
   primaryColor: 'terraRed',
 };
 
