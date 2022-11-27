478a387e130decebfa1c2ed29f1096d84e886bd5
Takashi Makimoto <mackie@beehive-dev.com>
2022-11-28 08:07:56 +0900

    メッセージを別コンポーネントに抽出 (#1)

--- src/components/menu/Introduction/Introduction.tsx
+++ src/components/menu/Introduction/Introduction.tsx
@@ -1,6 +1,7 @@
-import { Box, createStyles, Flex, Text } from '@mantine/core';
+import { Box, createStyles, Flex } from '@mantine/core';
 import ChoiceImage from './ChoiceImage';
 import PizzaImage from './PizzaImage';
+import Message from './Message';
 
 const useStyles = createStyles((theme) => ({
   choiceImage: {
@@ -19,17 +20,6 @@ const useStyles = createStyles((theme) => ({
       flexDirection: 'row',
     },
   },
-  message: {
-    color: theme.white,
-    fontSize: theme.fontSizes.lg,
-    fontWeight: 'bold',
-    [theme.fn.largerThan('sm')]: {
-      fontSize: theme.fontSizes.xl * 1.25,
-    },
-    [theme.fn.largerThan('md')]: {
-      fontSize: theme.fontSizes.xl * 1.5,
-    },
-  },
   messageBox: {
     marginTop: theme.spacing.xs,
     [theme.fn.largerThan('md')]: {
@@ -42,15 +32,6 @@ const useStyles = createStyles((theme) => ({
     flexGrow: 0,
     position: 'relative',
     width: 140,
-  },
-  subMessage: {
-    color: theme.white,
-    fontSize: theme.fontSizes.xs,
-    marginTop: theme.spacing.xs,
-    [theme.fn.largerThan('md')]: {
-      marginTop: 0,
-      textAlign: 'center',
-    },
   },
 }));
 
@@ -65,15 +46,7 @@ const Introduction = () => {
             <ChoiceImage />
           </Box>
           <Box className={classes.messageBox}>
-            <Text className={classes.message}>
-              <Text display="inline-block" span>
-                手作りの焼きたてのピザを
-              </Text>
-              <Text display="inline-block" span>
-                お持帰り頂けます
-              </Text>
-            </Text>
-            <Text className={classes.subMessage}>テイクアウトの箱代は、別途申し受けます。</Text>
+            <Message />
           </Box>
         </Box>
       </Box>
