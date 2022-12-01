2722d93691e98f16a7087e26e20c87e81e9eaeba
Takashi Makimoto <mackie@beehive-dev.com>
2022-12-01 23:37:16 +0900

    素材情報の背景を半透明に設定 (#2)

--- src/components/menu/MenuCard/Ingredients.tsx
+++ src/components/menu/MenuCard/Ingredients.tsx
@@ -1,4 +1,4 @@
-import { List, Text } from '@mantine/core';
+import { List, Text, Box } from '@mantine/core';
 
 type Props = {
   item: PizzaMenu;
@@ -6,22 +6,24 @@ type Props = {
 
 const MenuCard = ({ item }: Props) => {
   return (
-    <List center icon="●" size="xs" spacing="xs">
-      {item.ingredients.map((ingredient, index) => (
-        <List.Item
-          key={index}
-          sx={{
-            ':not(:first-of-type)': {
-              marginTop: 4,
-            },
-          }}
-        >
-          <Text span sx={{ textShadow: '1px 1px #fff' }}>
-            {ingredient}
-          </Text>
-        </List.Item>
-      ))}
-    </List>
+    <Box display="inline-block">
+      <List bg="rgba(255, 255, 255, 0.4)" center icon="●" p="xs" size="xs" spacing="xs">
+        {item.ingredients.map((ingredient, index) => (
+          <List.Item
+            key={index}
+            sx={{
+              ':not(:first-of-type)': {
+                marginTop: 4,
+              },
+            }}
+          >
+            <Text span sx={{ textShadow: '1px 1px #fff' }}>
+              {ingredient}
+            </Text>
+          </List.Item>
+        ))}
+      </List>
+    </Box>
   );
 };
 
